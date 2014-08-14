51e9c47568d0593e826f651705ac539ee7614d3d
Beret Parrot <danielspr3@gmail.com>
2014-08-14 23:04:56 +0300

    Hibernate settings, basic entities

--- juls-parent/juls-persist/src/main/java/com/juls/persist/App.java
+++ juls-parent/juls-persist/src/main/java/com/juls/persist/App.java
@@ -1,5 +1,6 @@
 package com.juls.persist;
 
+
 /**
  * Hello world!
  *
@@ -9,5 +10,6 @@ public class App
     public static void main( String[] args )
     {
         System.out.println( "Hello World!" );
+        new UserDAO().insert();
     }
 }
