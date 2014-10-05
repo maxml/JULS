5d308d36a8185be51061aac3bc509544a484b75f
Beret Parrot <danielspr3@gmail.com>
2014-10-06 01:05:16 +0300

    From good info page you can now access goods by category output page.

--- juls-parent/juls-rest/src/main/webapp/static/js/main.js
+++ juls-parent/juls-rest/src/main/webapp/static/js/main.js
@@ -6,16 +6,24 @@ $(document).ready(function(){
 		url: '../../user/curr',
 		contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
 		success: function(answer){
-			if (answer["regStatus"] == 0) {
-				$('#accStat').html('Account status: <font color="red">Unconfirmed</font>');
-			}
-			else {
-				$('#accStat').html('Account status: <font color="green">Registered</font>');
-				
-			}
+			
 			$('#userCabinetLink').html(cutEmailToUserName(answer["email"]));
 		}
 	});
+	if (window.localStorage.getItem("category")){
+		$('#searchquery').val("");
+		lastCategory = window.localStorage.getItem("category");
+		$.ajax({
+			type:'GET',
+			url: '../../goods/byCat/' + lastCategory,
+			async: true,
+			contentType: 'application/x-www-form-urlencoded; charset=UTF-8',
+			success:function(answer){
+				showGoods(answer);
+				window.localStorage.removeItem("category");
+			}
+		});
+	}
 });
 
 
